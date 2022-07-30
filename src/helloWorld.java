94d93edaafe4bc3867ef83f0886867bb6b76ec3b
daniel <daniellisboa2002@gmail.com>
2022-07-29 22:31:58 -0300

    primeiro commit

---
+++ src/helloWorld.java
@@ -0,0 +1,21 @@
+import model.Gato;
+
+public class helloWorld {
+    public static void main(String[] args) {
+//            int a = 2;
+//            int b = 4;
+//            System.out.println("Hello World!");
+//            System.out.println("A soma é " + (a + b));
+//            command + / para comentar linha(s) selecionada(s) com o mouse
+
+        Gato gato = new Gato();
+//            alt+enter para importar dependências
+//            control+alt+o para excluir imports não usados
+
+        System.out.println(gato);
+//            command+delete para deletar linha
+//            command+alt+L para formatar texto
+    }
+}
+
+
